019f786bd3a517a979ca4386789e994530fdd16a
JJ <jguaragno@mansueto.com>
2021-05-12 18:52:56 -0400

    add: initial spot

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,62 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const wordArray = [
+  'Bobby',
+  'Jerry',
+  'Phil',
+  'Mickey',
+  'Bill',
+  'Billy',
+  'Lesh',
+  'Garcia',
+  'Weir',
+  'Hart',
+  'Kreutzmann',
+  'Grateful Dead',
+  'wall of sound',
+  'sugaree'
+];
+
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  const paragraphsRequest = req.query.numParagraphs || 3;
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const fetchRandomWord = () => {
+    return wordArray[Math.floor(Math.random() * wordArray.length)];
+  }
+
+  const buildSentence = () => {
+    const maxSentenceCharacterCount = 100;
+    let currentCharacterCount = 0;
+    let sentenceWordArray = [];
+    while (currentCharacterCount < maxSentenceCharacterCount) {
+      const currentWord = fetchRandomWord();
+      sentenceWordArray.push(currentWord)
+      currentCharacterCount += currentWord.length;
+    }
+    const firstWord = sentenceWordArray.shift();
+    return `${capitalizeFirstLetter(firstWord)} ${sentenceWordArray.join (' ')}. `;
+  }
+
+  const buildParagraph = (asString = false) => {
+    let paragraphSentenceArray = [];
+    const numSentencesInParagraph = 4;
+    for (let index = 0; index < numSentencesInParagraph; index++) {
+      paragraphSentenceArray.push(buildSentence());
+    }
+    if(asString){
+      return paragraphSentenceArray.join(' ');
+    }
+    return paragraphSentenceArray;
+  }
+
+  let finalResponse = [];
+  for (let index = 0; index < paragraphsRequest; index++) {
+    finalResponse.push(buildParagraph());
+  }
+
+  res.status(200).json({ stuff: finalResponse })
 }
